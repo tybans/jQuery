ae5228039f33c771da7ee6285a73b8bcb0c408e9
Taiyab Ansari <taiyab0523@gmail.com>
2025-04-14 17:24:57 +0530

    practicing

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // This function runs when the document is fully loaded
+    $("h1").css("color", "blue");
+});
+// jQuery("h1").css("color", "red");
+// OR
+// $("h1").css("color", "blue");
